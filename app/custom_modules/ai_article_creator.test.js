28620a945643a5f796162b4715d47aca2cf087bc
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-04 16:29:47 +0000

    building test for the ai article creator module

--- app/custom_modules/ai_article_creator.test.js
+++ app/custom_modules/ai_article_creator.test.js
@@ -1,23 +1,58 @@
 import { expect, test } from 'vitest';
 const aiArticleCreator = require('./ai_article_creator');
 
+let testKeywords = [
+    'motorsport',
+    'video game',
+    'retrogaming',
+    'science fiction'
+];
+
+let testDescription = 'an article about the video game franchise wipeout';
+
+let testLanguage = 'fr';
+
 test('generate a correct prompt', async () => {
 
-    const testKeywords = [
-        'motorsport',
-        'video game',
-        'retrogaming',
-        'science fiction'
-    ];
+    const paramsHasKeywords = testKeywords.length !== 0;
+    const paramsHasDescription = testDescription !== '';
 
-    const testDescription = 'an article about the video game franchise wipeout';
+    const keywordsTxt = `use the following keywords: ${testKeywords}`;
+    const descriptionTxt = `use the following description: ${testDescription}`;
 
-    const testLanguage = 'fr';
+    const addParamInReq = `${paramsHasKeywords ? keywordsTxt : ''}${paramsHasKeywords && paramsHasDescription ? ' & ' : ''}${paramsHasDescription ? descriptionTxt : ''}`;
+    
+    let testRequest = `write an article optimized for search engine. to define the topics of the article and the lexical field, ${addParamInReq}. 
+        it should be written in html format, without the doctype, the head tag, or the html tag. you should use only text related tags, such as p, ul, ol, li and heading tags. the language of the article should be french.
+        the article should contain subtitles for each section.`;
 
     const prompt = aiArticleCreator.generatePrompt(testKeywords, testDescription, testLanguage);
 
+    console.log("prompt generated:")
     console.log(prompt);
 
+    console.log("test request:");
+    console.log(testRequest);
+
     await expect(prompt).toBeTypeOf('string'); 
+    await expect(prompt).toEqual(testRequest);
 
 });
+
+test('generate an article', async() => {
+
+    const prompt = aiArticleCreator.generatePrompt(testKeywords, testDescription, testLanguage);
+    let aiArticleResponse = null
+
+    try {
+
+        aiArticleResponse = await aiArticleCreator.generateArticle(prompt);
+        console.log(aiArticleResponse);
+
+    } catch(err) {
+
+        console.log(err)
+
+    }
+
+}, 15000);
